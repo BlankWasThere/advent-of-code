80f06d7a2829b380b0ed9abbecca42b31d0062c4
BlankWasThere <186826343+BlankWasThere@users.noreply.github.com>
2025-12-07 17:45:28 +0530

    feat: add solution to 2025/day_5 part 1

--- 2025/day_5/src/part_1.rs
+++ 2025/day_5/src/part_1.rs
@@ -1,3 +1,44 @@
+fn parse_input(input: &str) -> anyhow::Result<(Vec<(u64, u64)>, Vec<u64>)> {
+    let mut state = false; // false = ranges | true = values
+    let mut ranges = Vec::new();
+    let mut values = Vec::new();
+
+    for line in input.trim().lines() {
+        let line = line.trim();
+
+        // Separator for switching state
+        if line.is_empty() {
+            state = true;
+            continue;
+        }
+
+        match state {
+            // Read ranges
+            false => {
+                let Some((start, end)) = line.split_once("-") else {
+                    return Err(anyhow::anyhow!("Invalid line `{line}`: not a range."));
+                };
+
+                ranges.push((start.parse()?, end.parse()?));
+            }
+            true => {
+                values.push(line.parse()?);
+            }
+        }
+    }
+
+    Ok((ranges, values))
+}
+
 pub fn solve(input: &str) -> anyhow::Result<()> {
+    let (ranges, values) = parse_input(input)?;
+
+    let fresh_veggies_count = values
+        .into_iter()
+        .filter(|&n| ranges.iter().any(|&(start, end)| n >= start && n <= end))
+        .count();
+
+    println!("Answer: {fresh_veggies_count}");
+
     Ok(())
 }
