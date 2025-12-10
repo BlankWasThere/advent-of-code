15742d9153b42fb8d24098d87afea5e1a90ec0e8
BlankWasThere <186826343+BlankWasThere@users.noreply.github.com>
2025-12-11 00:53:20 +0530

    feat: add solution to 2025/day_9 part 1

--- 2025/day_9/src/part_1.rs
+++ 2025/day_9/src/part_1.rs
@@ -1,3 +1,38 @@
+fn parse_input(input: &str) -> anyhow::Result<Vec<(u64, u64)>> {
+    input
+        .trim()
+        .lines()
+        .filter_map(|s| {
+            let s = s.trim();
+            if !s.is_empty() { Some(s) } else { None }
+        })
+        .map(|s| {
+            let &[x, y] = s
+                .splitn(2, ',')
+                .map(|s| Ok(s.parse()?))
+                .collect::<anyhow::Result<Vec<_>>>()?
+                .as_slice()
+            else {
+                return Err(anyhow::anyhow!("Invalid grid index: `{s}`"));
+            };
+            Ok((x, y))
+        })
+        .collect()
+}
+
 pub fn solve(input: &str) -> anyhow::Result<()> {
+    let points = parse_input(input)?;
+
+    let mut max_area = 0;
+    for &x in &points {
+        for &y in &points {
+            if x != y {
+                max_area = max_area.max((x.0.abs_diff(y.0) + 1) * (x.1.abs_diff(y.1) + 1));
+            }
+        }
+    }
+
+    println!("Answer: {}", max_area);
+
     Ok(())
 }
